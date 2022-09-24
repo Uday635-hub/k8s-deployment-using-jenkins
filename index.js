c2fde48c6b2e5778f605935dc4e87f20dcbae811
Uday635-hub <108003647+Uday635-hub@users.noreply.github.com>
2022-09-25 01:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
-});
-
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "Server ready" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
